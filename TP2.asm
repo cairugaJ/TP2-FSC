4b41bfe634f6de72b95d1a5918d26bba466d15a9
Lienss <107154440+Lienss@users.noreply.github.com>
2022-11-03 21:35:07 -0300

    Update TP2.asm

--- TP2.asm
+++ TP2.asm
@@ -1,21 +1,3 @@
-# if (Prim > Ult)
-# return -1; // Valor não existe
-# else
-# { // Invariante: Se Valor existe em A,
-# // A[Prim]<=Valor<=A[Ult]
-# int Meio = (Prim + Ult)/2;
-# if (Valor == A[Meio])
-# return Meio; // Encontrou Valor em A[Meio]
-# else if (Valor<A[Meio])
-# return BinSearch(A, Prim, Meio-1, Valor);
-#// Recursão na metade inferior do vetor
-# else
-# return BinSearch(A, Meio+1, Ult, Valor);
-#// Recursão na metade superior do vetor
-# } // end else
-#} // end BinSearch
-#Gabriela Dellamora Paim, João Enrique Rego Cairuga e Luize Silveira Iensse 
-
 .globl
 .text  main
 .text VALOR_INEXISTENTE
@@ -34,6 +16,54 @@
 #	else
 #		return(binarySearch(A, high+1, low, key)
 #	}
+#}
+# 	
+main: 
+
+la $t0,A 
+lw $t1,0($t0) 
+la $t2,Prim
+lw $t3,0($t2) 
+la $t4,Ult 
+lw $t5,0($t4)
+la $t6,valor 
+lw $t7,0($t6)
+la $s0,Meio 
+lw $s1,0($s0)  
+
+bgt $t3,$t5, VALOR_INEXISTENTE  #acho que aqui tem que ser bgt pq deve ser maior apenas, não maior ou igual
+add $s1,$t3,$t5 
+div $s1,2
+beq $t1,$s1,RETORNA_MID 
+blt $t7,RETORNO_MID, BIANARYSEARCH 
+
+
+
+RETORNA_MID: 
+#valor do meio do vetor
+
+
+BYNARYSEARCH:
+#algoritmo para fazer a pesquina no vetor
+
+bge Prim, Ult, VALOR_INEXISTENTE
+add meio, Prim, Ult
+div meio, mid, 2
+beq meio, Valor, RETORNA_MID
+bge meio, Valor, binarySearch(A, Ult, Prim-1, Valor)
+bge key_value, mid, binarySearch(A, Ult+1, Prim, Valor)
+RETORNA_VALOR
+
+.data 
+
+A:      .word -5 -1 5 9 12 15 21 29 31 58 250 325	#Array 
+Prim:   .word 						#Sentinela de Início
+Ult:    .word 						#Sentinela de Fim
+Valor:  .word 						#Valor a ser buscado
+Meio:   .word 						#Valor do Meio
+
+
+
 #}
 # 	
 main: 
